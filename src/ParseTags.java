ff60403b5543b63fb272a62504539225c0dc895e
Luís Macedo <luisjrsm@hotmail.com>
2018-05-22 21:02:46 +0100

    Parser

---
+++ src/ParseTags.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class ParseTags here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ParseTags extends DefaultHandler{
+    public void startElement(String uri, String localName, String qName, Attributes attributes)
+            throws SAXException {
+        if (qName.equalsIgnoreCase("row")) {
+            String id = attributes.getValue("Id");
+            String rep = attributes.getValue("Reputation");
+            String date = attributes.getValue("CreationDate");
+            String name = attributes.getValue("DisplayName");
+            String bio = attributes.getValue("AboutMe");
+            
+            System.out.println("Id:"+id);
+            System.out.println("Reputation:"+rep);
+            System.out.println("CreationDate:"+date);
+            System.out.println("DisplayName:"+name);
+            System.out.println("About me:"+bio);
+        } 
+    }
+}
---
+++ src/TesteParser.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class TesteParser here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class TesteParser{
+    public static void main(String[] args){
+        xmlParser.parser(args[0]);
+    }
+}
